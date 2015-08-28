b3f5fd69b8951d1a2dd5a667f7a65823e767e68c
Jessica Jones <hello@heyjess.ca>
2015-08-28 12:51:39 -0400

    Refactored JS

---
+++ scripts/scripts-angular.js
@@ -0,0 +1,7 @@
+//Initialize Angular Application
+var app = angular.module('booze-app',[]);
+
+//Define Main Controller
+app.controller('MainCtrl', function() {
+    //Where Stuff Happens
+});
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,133 @@
 var app = {};
-var bookURL = "https://www.googleapis.com/books/v1/users/115753440391396010607/bookshelves/2/volumes";
+var bookURL = "";
 app.boozeURL = "http://lcboapi.com/products";
 app.boozeKey = "MDo2NTIyNTk3NC0zNjJlLTExZTUtYTg1Zi1kYjU1YzhlYzg2YmM6YXpwU0lhS3phNTdhVDJ4UVg1b0hUTUx6ODQ5UjNaNjhsZDE3";
 
+app.getBoozeAndBooks = function(){
+	// Get data for books from Google Books API
+	var booksCall = $.ajax({
+		url: bookURL,
+		type: "GET",
+		dataType: "json",
+		data: {
+			key: "AIzaSyDitBcEotbQY_BPq2qFEimwK9ugNGzK00g"
+		}
+	});
 
-// Collect data from the user
+	// Get data for booze from LCBO API
+	var boozeCall = $.ajax({
+		url: app.boozeURL,
+		type: "GET",
+		dataType: "jsonp",
+		headers : {
+			Authorization : "Token token=" + app.boozeKey
+		},
+		data: {
+			q : app.boozeType,
+			per_page: 100
+		}
+	});
+
+	// Get a random array item when AJAX request is called
+	function randomNumber(randomArray) {
+		return Math.floor(Math.random() * randomArray.length);
+	}
+
+	// When the calls are completed, retrieve a random book and booze and display on the page
+	$.when(booksCall, boozeCall).done(function(books, booze) {
+		var randomBook = randomNumber(books[0].items);
+		app.displayBook(books[0].items[randomBook].volumeInfo);
+
+		var randomBooze = randomNumber(booze[0].result);
+		app.displayBooze(booze[0].result[randomBooze]);
+	});
+};
+
+// Display API data on page
+app.displayBooze = function(data){
+	// Display booze title
+	$boozeTitle = $('<h4>');
+	$boozeTitle.text(data.name);
+
+	//If the result has a style, display booze style
+	if (data.style != null){
+		$boozeStyle = $('<h5>');
+		$boozeStyle.text(data.style);
+	} else {
+		$boozeStyle.text(" ");
+	}
+
+	//Display booze image
+	$boozeImageContainer = $('<div>').addClass('img-container');
+	$boozeImage = $('<img>').addClass('booze-img');
+	if(data.image_url != null) {
+		$boozeImage.attr('src', data.image_url);
+	} else {
+		$boozeImage.attr('src', 'images/noimage.jpg');
+	}
+	$boozeImageContainer.append($boozeImage);
+
+	//If the result has tasting notes, display booze tasting note
+	$boozeDescription = $('<p>');
+	
+	if (data.tasting_note != null){
+	// 	// Limit booze tasting note length to 200 characters
+		var boozeNote = data.tasting_note;
+		if(boozeNote.length > 200) {
+	    	boozeNote = boozeNote.substring(0,199)+"...";
+		}
+		$boozeDescription.text(boozeNote);
+	} else {
+		$boozeDescription.text(" ");
+	}
+
+	// Display booze link
+	var boozeLink = data.name.replace(/['\s]/g, '-');
+	var baseURL = "http://www.lcbo.com/lcbo/product/";
+	var productURL = baseURL + boozeLink + "/" + data.id;
+	$boozeLink = $('<a>');
+	$boozeLink.attr('href', productURL);
+	$boozeLink.html('See this booze on LCBO');
+	$('#booze').append($boozeTitle, $boozeStyle, $boozeImageContainer, $boozeDescription, $boozeLink);
+
+};
+
+app.displayBook = function(data) {
+	// If book has an image, display book data on the page
+	//Display book title
+	$bookTitle = $('<h4>');
+	$bookTitle.text(data.title);
+
+	//Display book author
+	$bookAuthor = $('<h5>');
+	$bookAuthor.text(data.authors);
+
+	//Display book thumbnail
+	$bookImageContainer = $('<div>').addClass('img-container');
+	$bookImage = $('<img>').addClass('book-img');
+	$bookImage.attr('src', data.imageLinks.thumbnail);
+	$bookImageContainer.append($bookImage);
+
+	//Display book description
+	if (data.description != null) {
+		$bookDescription = $('<p>');
+		$bookDescription.text(data.description);	
+	}
+
+	//Display book link
+	$bookLink = $('<a>');
+	$bookLink.attr('href', data.infoLink);
+	$bookLink.html('See this book on Google Books');
+	$('#book').append($bookTitle, $bookAuthor, $bookImageContainer, $bookDescription, $bookLink);
+}
+
+app.init = function(){
+	// Collect data from the user
 	$('.mood-btn').on('click', function(){
+		$('#book').empty();
+		$('#booze').empty();
 		// User selects a mood by clicking on a button
-		// Get the ID of the button they clicked and save it as a variable
+		// Get the data attribute of the button they clicked and save it as a variable
 		app.boozeType = $(this).data('boozekeyword');
 		app.bookType = $(this).data('bookkeyword');
 
@@ -38,119 +158,12 @@ app.boozeKey = "MDo2NTIyNTk3NC0zNjJlLTExZTUtYTg1Zi1kYjU1YzhlYzg2YmM6YXpwU0lhS3ph
 		$('html,body').animate({
 			scrollTop: $('#results').offset().top
 		}, 1000);
+
+		// When "Try Again" button is clicked, scroll to top
+		$('.btn').on('click', function(){
+			$("html, body").animate({ scrollTop: 0 }, "slow");
+		});
 	});
-	
-app.getBoozeAndBooks = function(){
-	// Get data for books from Google Books API
-	var booksCall = $.ajax({
-		url: bookURL,
-		type: "GET",
-		dataType: "json",
-		data: {
-			key: "AIzaSyDitBcEotbQY_BPq2qFEimwK9ugNGzK00g"
-		}
-	});
-
-	// Get data for booze from LCBO API
-	var boozeCall = $.ajax({
-		url: app.boozeURL,
-		type: "GET",
-		dataType: "jsonp",
-		headers : {
-			Authorization : "Token token=" + app.boozeKey
-		},
-		data: {
-			q : app.boozeType,
-			per_page: 100
-		},
-		error: function(){
-			$.ajax(this);
-		}
-	});
-
-	// Get a random array item when AJAX request is called
-	function randomNumber(randomArray) {
-		return Math.floor(Math.random() * randomArray.length);
-	}
-
-	// When the calls are completed, retrieve a random book and booze and display on the page
-	$.when(booksCall, boozeCall).done(function(books, booze) {
-		var randomBook = randomNumber(books[0].items);
-		app.displayInfo(books[0].items[randomBook].volumeInfo);
-
-		var randomBooze = randomNumber(booze[0].result);
-		app.displayInfo(booze[0].result[randomBooze]);
-	});
-};
-
-// Display API data on page
-app.displayInfo = function(data){
-	// If booze has an image, display booze data on the page
-	if (data.image_url != null) {
-		// Insert booze result title on page
-		var boozeTitle = data.name;
-		$('#booze-title').text(boozeTitle);
-
-		//If the result has a style, insert booze result style on page
-		if (data.style != null){
-			var boozeStyle = data.style;
-			$('#booze-style').text(boozeStyle);
-		} else {
-			$('#booze-style').html('<br>');
-		}
-		//Insert booze result image on page
-		$('#booze-img').attr('src', data.image_url);
-
-		// Insert booze info link on page
-		var boozeLink = data.name.replace(/['\s]/g, '-');
-		var baseURL = "http://www.lcbo.com/lcbo/product/";
-		var productURL = baseURL + boozeLink + "/" + data.id;
-		$('#booze-info').attr('href', productURL);
-
-		//If the result has tasting notes, insert booze result tasting note on page
-		if (data.tasting_note != null){
-			var boozeNote = data.tasting_note;
-			// Limit booze description length to 200 characters
-			if(boozeNote.length > 200) {
-		    	boozeNote = boozeNote.substring(0,199)+"...";
-			}
-			$('#booze-description').text(boozeNote);
-		} else {
-			$('#booze-description').html('<br>');
-		}
-	};
-
-	// If book has an image, display book data on the page
-	if (data.imageLinks != null) {
-		var bookTitle = data.title;
-		$('#book-title').text(bookTitle);
-
-		var bookAuthor = data.authors;
-		$('#book-author').text(bookAuthor);
-
-		var bookInfo = data.infoLink;
-		$('#book-info').attr('href', bookInfo);
-
-		var bookImage = data.imageLinks.thumbnail;
-		$('#book-img').attr('src', bookImage);
-		if (data.description != null) {
-			var bookDescription = data.description;
-			// Limit book description length to 200 characters
-			if(bookDescription.length > 200) {
-		    	bookDescription = bookDescription.substring(0,199)+"...";
-			}
-			$('#book-description').text(bookDescription);		
-		}
-	}
-};
-
-// When "Try Again" button is clicked, refresh page
-	$('.btn').on('click', function(){
-		location.reload();
-	});
-
-app.init = function(){
-	app.getBoozeAndBooks();
 };
 
 $(function(){
